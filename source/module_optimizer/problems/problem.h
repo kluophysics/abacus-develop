b601610efb69a074a03258d09e01e7302e7f4b56
Kai Luo <kluophysics@gmail.com>
2025-01-08 08:35:51 +0800

    minor fixes

--- source/module_optimizer/problems/problem.h
+++ source/module_optimizer/problems/problem.h
@@ -8,8 +8,8 @@
 namespace Module_Optimizer
 {
 
-    class Manifold;
-    
+    // class Manifold;
+
     // class Problem; when defining the problem, one has to give initial X0 and direction d0
     template <typename T>
     class Problem
@@ -28,7 +28,7 @@ namespace Module_Optimizer
 
         // Euclidean gradient of f, which has to be defined for each problem
         // virtual ManifoldPoint grad(const ManifoldPoint& x) =0;
-        virtual ManifoldPoint grad();
+        virtual ManifoldVector grad();
 
         // evaluate objective function and gradient at the same time.
         virtual void evaluate_obj_and_grad();
